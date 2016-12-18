e7dc7aaba677e5f88f118c21616ec7c63531621a
Arash Tavallay Rudsari <rushymek@gmail.com>
2016-12-18 20:52:20 +0100

    added env variables to create.sh

--- create.sh
+++ create.sh
@@ -1,4 +1,4 @@
-docker-compose run web rails new . --force --database=postgresql --skip-bundle
+docker-compose run web rails new . --force --database=postgresql --skip-bundle -e ROR_UID=${UID} -e APPNAME=$1
 docker-compose build
 cp database.yml config/
 docker-compose up
